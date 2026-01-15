afd98645fee5a6e9698fd0291d8363e5d7cc7ffb
vbvss199 <vbvss1999@gmail.com>
2026-01-15 10:40:29 -0500

    added some shit

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
