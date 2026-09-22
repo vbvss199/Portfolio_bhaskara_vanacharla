af65bf103b587618b243f2a73a5bb50f74e39139
vbvss199 <vbvss1999@gmail.com>
2026-09-22 12:26:19 -0500

    changed the resume '

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -16,5 +16,5 @@ export const personalData = {
   leetcode: "https://leetcode.com/u/22M1yfqB57/",
   devUsername: "bhaskaravanacharla",
   resume:
-    "https://drive.google.com/file/d/1eKqTJTDSATyrPoG6jfCRDsmJoOlmdzwK/view?usp=sharing",
+    "https://drive.google.com/file/d/1Iut_ilplwn3pKuMatYyqrAKNwYHYDmgn/view?usp=sharing",
 };
